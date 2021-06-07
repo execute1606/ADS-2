b79ce89a951b1671daa62f78eaa20d51212efd97
execute1606 <72761350+execute1606@users.noreply.github.com>
2021-06-07 16:53:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,52 +1,47 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(int* arr, int len, int value)
+{
     int chet = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
             if ((arr[i] + arr[j]) == value)
                 chet++;
         }
     }
     return chet;
 }
-
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(int* arr, int len, int value)
+{
     int chet = 0;
-    for (int i = len - 1; i > 0; i--) {
-        if (arr[i] <= value) {
-            for (int j = 0; j < i; j++) {
-                if ((arr[i] + arr[j]) == value)
-                    chet++;
-            }
+    for (int i = 0; i < len - 1; i++)
+        for (int j = len - 1; j < len; j--)
+        {
+            if (i == j)
+                return chet;
+            else if ((arr[i] + arr[j]) == value)
+                chet++;
         }
-    }
     return chet;
 }
 
-int countPairs3(int* arr, int len, int value) {
+int countPairs3(int* arr, int len, int value)
+{
     int chet = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int left = i + 1, right = len, twoElement = value - arr[i];
-        while (left < right) {
-            int cent = (left + right) / 2;
-            if (arr[cent] == twoElement) {
-                while ((arr[cent - 1] == twoElement) && (cent > left)) {
-                    cent--;
-                }
-                while (arr[cent] == twoElement) {
-                    cent++;
-                    chet++;
-                }
-                break;
-            }
-            else if (arr[cent] > twoElement) {
-                right = cent;
-            }
-            else {
-                left = cent + 1;
-            }
-        }
+    int siz = 0;
+    for (int z = 1; z < len; z++)
+    {
+        if ((arr[z] + arr[0]) == value)
+            siz = z;
+    }
+
+    int twoElement = value - arr[0];
+    for (int i = 0; i <= siz; i++)
+    {
+        if (arr[i] == twoElement)
+            chet++;
     }
     return chet;
 }
